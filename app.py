4fa14d3f3a35050771c323833eb79520f4846081
Munzza <zm.javadeveloper@gmail.com>
2018-01-19 06:31:54 -0800

    fixed not importing db when running the app

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ from resources.item import Item, ItemList
 from resources.store import Store,StoreList
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get('DATABASE-URL', 'sqlite:///data.db')
+
+app.config['DEBUG'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'zinb'
 api=Api(app)
@@ -26,3 +29,10 @@ api.add_resource(StoreList,'/stores')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=4999)
